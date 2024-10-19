7787676d1149eb9d0924a3365d18a80d3044713e
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-10-19 22:04:36 +0900

    finall

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 require("dotenv").config();
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 console.log("mongouri", MONGODB_URI_PROD)
 const app = express();
-app.use(cors({ origin: 'https://todo-demo3.netlify.app' }));
+app.use(cors());
+const cors = require("cors");
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
