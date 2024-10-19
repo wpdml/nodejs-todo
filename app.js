a6c1c8247981958522866abf256746d354e3503c
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-10-19 23:30:38 +0900

    2

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const cors = require("cors");
-
 require("dotenv").config();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://todo-demo3.netlify.app"
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
 const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 console.log("mongouri", MONGODB_URI_PROD)
 const app = express();
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
 
