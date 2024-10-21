b4fb10ebe0edada496040071d5c7945bc5e287bf
이제의 <jeui@ije-uis-MacBook-Air.local>
2024-10-22 06:19:50 +0900

    new

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -32,7 +32,7 @@ userController.loginWithEmail = async (req, res) => {
         return res.status(200).json({ status: "success!", user, token });
       }
     }
-    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다");
+    throw new Error("Email or password does not match");
   } catch (error) {
     res.status(400).json({ status: "fail", message:error.message });
   }
